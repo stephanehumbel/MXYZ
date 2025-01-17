e0f66cca22413aeca5b50f6ccf09f775102d4eae
stephanehumbel <82397859+stephanehumbel@users.noreply.github.com>
2025-01-17 19:07:40 +0100

    Update MXYZ.py for g16 and NImag

--- MXYZ.py
+++ MXYZ.py
@@ -10,8 +10,64 @@ numeroZ_to_symbol = {
     31: 'Ga', 32: 'Ge', 33: 'As', 34: 'Se', 35: 'Br', 36: 'Kr',
     37: 'Rb', 38: 'Sr', 39: 'Y', 40: 'Zr', 41: 'Nb', 42: 'Mo',
     43: 'Tc', 44: 'Ru', 45: 'Rh', 46: 'Pd', 47: 'Ag', 48: 'Cd',
-    49: 'In', 50: 'Sn', 51: 'Sb', 52: 'Te', 53: 'I', 54: 'Xe'
+    49: 'In', 50: 'Sn', 51: 'Sb', 52: 'Te', 53: 'I', 54: 'Xe',
+    78: 'Pt'
 }
+def is_num(txt):
+    chiffres = "0123456789"
+    return all([car in chiffres for car in txt])
+
+       ##   print_xyz_displ(atom_displ,Nfreq)
+
+def read_atom_displ(lines, start_line,size):
+    # Dictionnaire pour stocker les deplacements des atomes
+    atom_displ = {}
+    Ifreq=0
+    ifreq=0
+    # Parcourir les lignes à partir de la ligne de départ
+    for i in range(start_line+5, start_line+5+size):
+        line = lines[i].strip()
+#        print("sub",line)
+        # Arrêter si on atteint les tirets
+        if '-----' in line:
+            break
+
+        # Ignorer les lignes vides
+        if not line:
+            continue
+
+        # Diviser la ligne en colonnes
+        columns = line.split()
+        print("sub",columns,len(columns))
+
+        # Récupérer les informations sur l'atome
+        atom_number = int(columns[0])
+        numeroZ = int(columns[1])  # Utiliser la colonne 2 comme numeroZ
+        x_coord = float(columns[2])
+        y_coord = float(columns[3])
+        z_coord = float(columns[4])
+        ifreq+=1
+        if len(columns) >= 5 and ifreq < Nfreq:
+           x_coord2 = float(columns[5])
+           y_coord2 = float(columns[6])
+           z_coord2 = float(columns[7])
+           ifreq+=1
+           if len(columns) >= 8 and ifreq < Nfreq:
+              x_coord3 = float(columns[8])
+              y_coord3 = float(columns[9])
+              z_coord3 = float(columns[10])
+              ifreq+=1
+        print(Ifreq,x_coord2)
+        Ifreq=Ifreq+ifreq
+        ifreq=0
+        # Stocker les displ dans le dictionnaire avec le numéro d'atome et numeroZ
+        atom_displ[Ifreq] = {'atom_number': atom_number,'numeroZ': numeroZ, 'x': x_coord, 'y': y_coord, 'z': z_coord}
+        atom_displ[Ifreq+1] = {'atom_number': atom_number,'numeroZ': numeroZ, 'x': x_coord2, 'y': y_coord2, 'z': z_coord2}
+        atom_displ[Ifreq+2] = {'atom_number': atom_number,'numeroZ': numeroZ, 'x': x_coord3, 'y': y_coord3, 'z': z_coord3}
+#       print(atom_displ)
+    return atom_displ
+
+
 
 def read_atom_coordinates(lines, start_line):
     # Dictionnaire pour stocker les coordonnées des atomes
@@ -53,10 +109,18 @@ def find_geometry_start(nom_fic):
     # Lire toutes les lignes du fichier
     with open(nom_fic, 'r') as file:
         lines = file.readlines()
-
-    # Chercher le motif marquant le début des coordonnées de la géométrie
+    start_indices=0
+    pos_Done=0
+    pos_NImag=0
+    pos_Frequencies=0
+    # Chercher le motif marquant le début des coordonnées de la géométrie i et autres infos
     start_indices = [i +3 for i, line in enumerate(lines) if "Coordinates (Angstroms)" in line]
-    return start_indices
+    pos_Done = [i for i, line in enumerate(lines) if "SCF Done:" in line]
+    pos_NImag = [i for i, line in enumerate(lines) if "imaginary frequencies (" in line]
+    if pos_NImag !=0 :
+      pos_Frequencies = [i for i, line in enumerate(lines) if "Frequencies -- " in line]
+#    print(start_indices,pos_Done,pos_NImag,pos_Frequencies)
+    return start_indices,pos_Done, pos_NImag,pos_Frequencies
 
 def process_arguments(nom_fic, option=None):
     # Afficher le nom du fichier
@@ -73,16 +137,14 @@ def print_xyz_coordinates(atom_coordinates,opt):
     Parameters:
     - atom_coordinates (dict): Dictionary containing atom coordinates.
     """
-    if opt == '':
-       print("{:<4} {:>12} {:>12} {:>12}".format("Atom", "X", "Y", "Z"))
-       print("-" * 42)  # Separator line
+    #if opt == '':
+    #   print("{:<4} {:>12} {:>12} {:>12}".format("Atom", "X", "Y", "Z"))
+    #   print("-" * 42)  # Separator line
 
     for atom_number, info in atom_coordinates.items():
         symbol = info['symbol']
         x_coord, y_coord, z_coord = info['x'], info['y'], info['z']
-        print("{:<4} {:12.6f} {:12.6f} {:12.6f}".format(symbol, x_coord, y_coord, z_coord))
-
-
+        print("{:<4} {:12.5f} {:12.5f} {:12.5f}".format(symbol, x_coord, y_coord, z_coord))
 
 if __name__ == "__main__":
     # Vérifier le nombre d'arguments
@@ -97,17 +159,50 @@ if __name__ == "__main__":
     else:
         print("Usage: {} <nom_fichier> [option]".format(sys.argv[0]))
 
-    start_indices = find_geometry_start(nom_fic)
-#    print("Indices des lignes de début de géométrie:", start_indices)
-    
+    start_indices,pos_Done,pos_NImag,pos_Frequencies = find_geometry_start(nom_fic)
+#    print("sort",start_indices,pos_Done,pos_NImag,pos_Frequencies)
+
     # Lire toutes les lignes du fichier
     with open(nom_fic, 'r') as file:
         lines = file.readlines()
-
+    if len(pos_Frequencies)!=0:
+      if len(pos_NImag)!=0:
+        NImag=int(lines[pos_NImag[0]].split()[1])
+      else:
+        NImag=0
+    else:
+        NImag=-1
     # Call read_atom_coordinates function for each start index
     for start_index in start_indices:
         atom_coordinates = read_atom_coordinates(lines, start_index)
         # print("Atom Coordinates:", atom_coordinates)
-
+    if option == '':
+       #print(pos_Done)
+       if len(pos_Done)!=0:
+          line=lines[int(pos_Done[len(pos_Done)-1])]
+          energy=line.split()[4]
+       else :
+          energy='00'
+       print(len(atom_coordinates))
+       print(nom_fic,"energy=",energy,end='')
+       if NImag >=0 : 
+            print("  NImag=",NImag)               
+       else :
+            print(" No Freq")
     print_xyz_coordinates(atom_coordinates,option)
+    if len(pos_Frequencies) !=0 :
+       Nfreq=min(NImag,3)
+       print("------",Nfreq       , "imaginary freq  collected ")
+       try: 
+          Nfreq=int(input("----- combien a traiter? (enter par default)"))
+       except:
+          pass
+       print ("traite",Nfreq)
+       for start_index in pos_Frequencies:
+          size=len(atom_coordinates)
+          atom_displ = read_atom_displ(lines, start_index,size)
+          print(atom_displ[0])
+       print_xyz_displ(atom_coordinates,pos_Frequencies,Nfreq)
+       
+   
 
